a60da0af7bccd9ccb62774fc53dc9f89aa36ee08
farhantaufiks <farhantaufiks@gmail.com>
2021-02-26 07:06:44 +0700

    fungsi update add dan delete gabung serta rapihin diki

--- index_edit.js
+++ index_edit.js
@@ -9,37 +9,53 @@ var reviews = [
     },
 ]
 
+
+const addButton = document.getElementById('form-button');
+addButton.addEventListener('click', addReview);
 const konten = document.getElementById("listReview")
 konten.addEventListener('click', deletEditReview)
-//
-
-generateCard(reviews)
-
-const edit = document.getElementsByClassName("edit")
-edit.addEventListener("click", editReview)
-// if ()
 
 
-function mulai(event) {
-    event.preventDefault()
-    generateCard(reviews)
-    document.getElementById('mulai').style.display = 'none';
+render(reviews)
+
+// const delButton = document.getElementById('reviewsList');
+// delButton.addEventListener('click', hapus);
+
+// function hapus(event) {
+//   const item = event.target;
+//   if (item.classList[0] === 'delet') {
+//     const comment = item.parentElement;
+//     comment.remove()
+//   }
+
+// }
+
+function addReview(event){
+    // clearArr()
+    event.preventDefault();
+    let obj = {
+        name : document.getElementById("name").value,
+        review : document.getElementById("review").value
+    }
+    reviews.push(obj)
+    console.log(reviews);
+    clearArr();
+    render(reviews);
 }
 
-function generateCard(reviews) {
-    console.log(reviews)
-    for (let i = 0; i < reviews.length; i++) {
-        // console.log('pokemons')
-        let cardContainer = document.createElement('div')
-        cardContainer.classList.add('card')
-        console.log('cardContainer')
-        let namaPokemon = document.createElement('h2')
-        namaPokemon.innerText = reviews[i].name
-        cardContainer.appendChild(namaPokemon)
 
-        let tipePokemon = document.createElement('p')
-        tipePokemon.innerText = reviews[i].review
-        cardContainer.appendChild(tipePokemon)
+function render(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+
+        let cardContainer = document.createElement('ul')
+        cardContainer.classList.add('card')
+
+
+        let data = arr[i]
+        let listElement = document.createElement('li')
+        listElement.innerText = `${data.name}: ${data.review}`;
+        cardContainer.appendChild(listElement)
 
         let editButton = document.createElement('button')
         editButton.innerText = 'Edit'
@@ -60,7 +76,7 @@ function generateCard(reviews) {
     }
 }
 
-function clearCard() {
+function clearArr() {
     konten.innerHTML = ''
 }
 function CloseInput() {
@@ -72,40 +88,38 @@ function deletEditReview(event) {
     const idx = Number(event.srcElement.attributes.value.value)
     if (event.srcElement.attributes.class.value == 'delet') {
         reviews.splice(idx, 1)
-        clearCard()
-        console.log(reviews)
-        generateCard(reviews)
+        clearArr()
+
+        render(reviews)
     } else if (event.srcElement.attributes.class.value == 'edit') {
-        // console.log(event.srcElement.attributes.class.value)
-        // console.log(reviews[idx].name)
+
         document.getElementById('spoiler').style.display = 'block';
         let el = document.getElementById('edit-name');
         let el1 = document.getElementById('edit-text');
-        // console.log(el)
+
         // Display value in the field
         el.value = reviews[idx].name
         el1.value = reviews[idx].review
         // Display fields
-        // console.log(el.value)
-        // self = this;
+
 
         document.getElementById('saveEdit').onsubmit = function (event) {
             // Get value
             event.preventDefault()
-            console.log("keprint")
+
             // var country = el.value;
             let current = {
                 name: el.value,
                 review: el1.value,
             }
-            console.log(current)
+
             // Edit value
             reviews.splice(idx, 1, current);
             console.log(reviews)
             // Display the new list
-            // console.log(current)
-            clearCard()
-            generateCard(reviews);
+
+            clearArr()
+            render(reviews);
             // Hide fields
             CloseInput();
         };
