a1c5e3d72cde2c62ef525df7efac82c5bba57d0d
Andi Javier Lafiedrido <andijavier442@gmail.com>
2021-02-26 07:32:12 +0700

    revised

--- index_create.js
+++ index_create.js
@@ -12,6 +12,25 @@ function hapus(event) {
   const item = event.target;
   if (item.classList[0] === 'delete') {
     const comment = item.parentElement;
+    let find = comment.querySelector('.li-review')
+    let data = find.innerHTML
+    console.log(find)
+    let newReviews = []
+    let temp = ''
+    for (let i = 0; i < data.length; i++) {
+        temp += data[i]
+        // console.log(data[i])
+        if (data[i] === ':') {
+          break;
+        }
+      }
+    for (let i = 0; i < reviews.length; i++) {
+        if (temp !== reviews[i].nama) {
+            newReviews.push(reviews[i])
+        }
+    }
+    reviews = newReviews
+    console.log(reviews)
     comment.remove()
   }
 }
@@ -36,11 +55,16 @@ function render(arr){
     // const konten = document.getElementById('reviewsList');
     for(let i = 0; i < arr.length; i++){
         // console.log(arr[i]);
+        // create div
+        let comment = document.createElement('div')
+        comment.classList.add('comment')
+        // create li
         let data = arr[i];
         let listElement = document.createElement('li');
         listElement.classList.add('li-review');
         listElement.innerText = `${data.nama}: ${data.review}`;
-        console.log(listElement);
+        comment.appendChild(listElement)
+        // console.log(listElement);
         //adding edit button
         let editIcon = document.createElement('button');
         let deleteIcon = document.createElement('button');
@@ -53,9 +77,9 @@ function render(arr){
         deleteIcon.setAttribute('type', 'submit')
         editIcon.setAttribute('type', 'submit')
 
-        listElement.appendChild(editIcon);
-        listElement.appendChild(deleteIcon);
+        comment.appendChild(editIcon);
+        comment.appendChild(deleteIcon);
 
-        document.getElementById('reviewsList').appendChild(listElement);
+        document.getElementById('reviewsList').appendChild(comment);
     }
 }
